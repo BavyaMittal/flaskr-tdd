00592c2a5ac2f55bf572b06dc2b319ffff5df9a9
BavyaMittal <bavyamittal@gmail.com>
2024-10-03 17:52:52 -0400

    connected database

--- project/app.py
+++ project/app.py
@@ -1,8 +1,8 @@
 import sqlite3
+import os
 from pathlib import Path
 
-from flask import Flask, g, render_template, request, session, \
-                  flash, redirect, url_for, abort, jsonify
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 
@@ -20,7 +20,14 @@ DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+
+url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = url
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
